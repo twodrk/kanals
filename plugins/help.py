7e0dafc42429cb92aaee03c87b468ac086e3cbaa
Two? <90793941+twodrk@users.noreply.github.com>
2022-01-10 19:19:53 +0400

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -42,10 +42,10 @@ def map(pos):
             [InlineKeyboardButton(text = '-->', callback_data = "help+2")]
         ]
     elif(pos==len(tr.HELP_MSG)-1):
-        url = "https://github.com/viperadnan-git/force-subscribe-telegram-bot/issues/new"
+        url = "https://t.me/lieblogs"
         button = [
-            [InlineKeyboardButton(text = 'Support Chat', url="https://t.me/ViperCommunity")],
-            [InlineKeyboardButton(text = 'Feature Request & Issues', url=url)],
+            [InlineKeyboardButton(text = '👤 Sahibim', url="https://t.me/ViperCommunity")],
+            [InlineKeyboardButton(text = '💌 Blog', url=url)],
             [InlineKeyboardButton(text = '<--', callback_data = f"help+{pos-1}")]
         ]
     else:
