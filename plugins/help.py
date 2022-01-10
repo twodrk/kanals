6d231bdc5d0873f5760e236ab8655b954482bc7c
Two? <90793941+twodrk@users.noreply.github.com>
2022-01-10 19:21:01 +0400

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -44,7 +44,7 @@ def map(pos):
     elif(pos==len(tr.HELP_MSG)-1):
         url = "https://t.me/lieblogs"
         button = [
-            [InlineKeyboardButton(text = '👤 Sahibim', url="https://t.me/ViperCommunity")],
+            [InlineKeyboardButton(text = '👤 Owner', url="https://t.me/ViperCommunity")],
             [InlineKeyboardButton(text = '💌 Blog', url=url)],
             [InlineKeyboardButton(text = '<--', callback_data = f"help+{pos-1}")]
         ]
